91e926ba92a2a779e89d485513378736cfffad80
AIR-CL <2016787823@qq.com>
2020-11-13 15:03:06 +0800

    创建项目后台界面4

--- src/main/java/fhq/mapper/UserMapper.java
+++ src/main/java/fhq/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package fhq.mapper;
 
+import fhq.pojo.User;
+
+//用户模块持久层
 public interface UserMapper {
+    //根据用户名查询当前用户信息
+    User findUserByUserName(String username);
+    int regUser(User user);
 }
--- src/main/java/fhq/pojo/User.java
+++ src/main/java/fhq/pojo/User.java
@@ -2,7 +2,114 @@ package fhq.pojo;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class User {
+import java.io.Serializable;
 
+@Component
+public class User implements Serializable {
+    private Integer userId;
+    private String userName;
+    private String userPassword;
+    private Integer userVip;//VIP等级
+    private String userFace;//用户头像地址
+    private String salt;//盐值
+    private Integer pushMoney;//充值金额
+    private String email;
+    private String sex;//性别
+    private String tel;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Integer getUserVip() {
+        return userVip;
+    }
+
+    public void setUserVip(Integer userVip) {
+        this.userVip = userVip;
+    }
+
+    public String getUserFace() {
+        return userFace;
+    }
+
+    public void setUserFace(String userFace) {
+        this.userFace = userFace;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public Integer getPushMoney() {
+        return pushMoney;
+    }
+
+    public void setPushMoney(Integer pushMoney) {
+        this.pushMoney = pushMoney;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", password='" + userPassword + '\'' +
+                ", userVip=" + userVip +
+                ", userFace='" + userFace + '\'' +
+                ", salt='" + salt + '\'' +
+                ", pushMoney=" + pushMoney +
+                ", email='" + email + '\'' +
+                ", sex='" + sex + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
 }
