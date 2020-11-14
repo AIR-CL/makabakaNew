98d350427018519f9a42b03bfa782ebd8d25704a
AIR-CL <2016787823@qq.com>
2020-11-14 20:43:44 +0800

    傅聪登录注册1

--- src/main/java/fhq/controller/UserController.java
+++ src/main/java/fhq/controller/UserController.java
@@ -1,12 +1,21 @@
 package fhq.controller;
 
+import fhq.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    //注入业务层对象
+    @Autowired
+    private UserService userService;
 
     //跳转到注册页面
     @GetMapping("/reg")
@@ -19,7 +28,33 @@ public class UserController {
     public String toLogPage(){
         return "web/login";
     }
+    @PostMapping("/checkUsername")
+    @ResponseBody//将返回值转成json数据
+    public Map<String,Object> checkUsername(String username){
+        //使用业务层对象调用业务层方法
+        Map<String, Object> map = userService.checkUserName(username);
+        return map;
+    }
+
+    //处理注册
+    @PostMapping("/toReg")
+    @ResponseBody//将返回值转成json数据
+    public Map<String,Object> toReg(String username,String password){
+
+        Map<String, Object> map = userService.toReg(username, password);
 
 
-
+        return map;
+    }
+    //处理登陆
+    @PostMapping("toLog")
+    @ResponseBody
+    public Map<String,Object> toLog(String username,String password){
+        Map<String, Object> map = userService.toLog(username, password);
+        return map;
+    }
+    @GetMapping("/toindex")
+    public String toindex(){
+        return "controlHtml/index";
+    }
 }
--- src/main/java/fhq/mapper/UserMapper.java
+++ src/main/java/fhq/mapper/UserMapper.java
@@ -1,17 +1,15 @@
 package fhq.mapper;
 
 import fhq.pojo.User;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 //用户模块持久层
-@Repository
 public interface UserMapper {
     //根据用户名查询当前用户信息
     User findUserByUserName(String username);
+    //添加新用户
     int regUser(User user);
-
+    //根据用户名和密码查询用户信息
+    User loginCheck(@Param("username") String username,@Param("password") String password);
 
 }
