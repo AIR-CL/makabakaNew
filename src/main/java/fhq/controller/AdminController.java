66b443971849b6f2fb0814471ec13114cd566f78
StevenQhy <66714578@qq.com>
2020-11-18 15:26:28 +0800

    qhy提交123123

--- src/main/java/fhq/controller/AdminController.java
+++ src/main/java/fhq/controller/AdminController.java
@@ -1,6 +1,5 @@
 package fhq.controller;
 
-
 import fhq.pojo.User;
 import fhq.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,106 +23,37 @@ public class AdminController {
     public String toIndex(){
         return "controlHtml/index";
     }
-    //根据id查询
-    @PostMapping("/findUserById")
-    @ResponseBody
-    public User findUserById(Integer userId){
-        User user = adminService.findUserByIdInfo(userId);
-        return user;
-    }
 
-
-    //查询总记录数
-    @GetMapping("/findUsersCount")
+    //查询所有用户
+    @GetMapping("/findUsers")
     @ResponseBody
-    public Integer findUsersCount(){
+    public List<User> findAllUsers(){
         //调用业务层的查询方法
-        Integer totalPage = adminService.findTotalPage();
-        return totalPage;
-    }
-    //模糊查询总记录数
-    @PostMapping("/findUsersLikeCount")
-    @ResponseBody
-    public Integer findUsersLikeCount(Integer userId,String userName,Integer userVip,Integer loPh,Integer hiPh){
-
-        Integer totalPage = adminService.findUsersCountInfo(userId, userName, userVip, loPh, hiPh);
-
-        return totalPage;
-    }
-    //模糊查询分页
-    @PostMapping("/findUsersLikeByPage")
-    @ResponseBody
-    public List<User> findUsersLike(Integer userId,String userName,Integer userVip,Integer loPh,Integer hiPh,Integer currentPage,HttpSession session){
-        //设置当前页面，方便进行下一页上一页操作
-        session.setAttribute("currentPage",currentPage);
-        List<User> users = adminService.findUsersLikeByPageInfo(userId, userName, userVip, loPh,hiPh,currentPage);
+        List<User> users = adminService.findAllUsersInfo();
         return users;
     }
-    //分页查询
-    @PostMapping("/findUsersByPage")
+    //模糊查询
+    @PostMapping("/findUsersLike")
     @ResponseBody
-    public List<User> findUsersByPage(Integer currentPage,HttpSession session){
-        //设置当前页面，方便进行下一页上一页操作
-        session.setAttribute("currentPage",currentPage);
-        List<User> users = adminService.findUsersByPageInfo(currentPage);
+    public List<User> findUsersLike(Integer userId,String userName,Integer userVip,Integer loPh,Integer hiPh){
+
+        List<User> users = adminService.findUsersLikeInfo(userId, userName, userVip, loPh,hiPh);
         return users;
     }
-
-
-
 
     //用户修改
-   @PostMapping("/updateUser")
+    @PostMapping("/updateUser")
     @ResponseBody
-    public Map<String,Object> updateUser(String userName,Integer userVip,Integer pushMoney,Integer userId){
-       Map<String, Object> map = adminService.updateUserInfo(userId, userName, userVip, pushMoney);
-       return map;
-   }
+    public Map<String,Object> updateUser(Integer userId, Integer userVip, Integer pushMoney){
+        Map<String, Object> map = adminService.updateUserInfo(userId, userVip, pushMoney);
+
+        return map;
+    }
     //用户删除
     @PostMapping("/deleteUser")
     @ResponseBody
     public  Map<String,Object> deleteUser(Integer id){
-
         Map<String, Object> map = adminService.deleteUserInfo(id);
         return map;
     }
-
-    //跳转到修改页面
-    @GetMapping("/toUpdateUserPage")
-    public String toUpdatePage(Integer userId, HttpSession session){
-        //设定一个seesion用于发送ajax请求
-        session.setAttribute("userId",userId);
-        return "/controlHtml/updatePage";
-
-    }
-    //获取session
-    @GetMapping("getSession")
-    @ResponseBody
-    public Map<String,Object> getSession(HttpSession session){
-        //获取当前页
-        Integer currentPage=(Integer) session.getAttribute("currentPage");
-        //获取总页数
-        Integer totalPage = adminService.findTotalPage();
-        Map<String,Object> map=new HashMap<String, Object>();
-        map.put("currentPage",currentPage);
-        map.put("totalPage",totalPage);
-
-        return map;
-    }
-
-    //模糊getSession
-    @GetMapping("getLikeSession")
-    @ResponseBody
-    public Map<String,Object> getLikeSession(Integer userId,String userName,Integer userVip,Integer loPh,Integer hiPh,HttpSession session){
-        //获取当前页面
-        Integer currentPage=(Integer) session.getAttribute("currentPage");
-        //获取总页数
-        Integer totalPage = adminService.findUsersCountInfo(userId,userName,userVip,loPh,hiPh);
-        Map<String,Object> map=new HashMap<String, Object>();
-        map.put("currentPage",currentPage);
-        map.put("totalPage",totalPage);
-
-        return map;
-    }
-
 }
--- src/main/java/fhq/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/fhq/service/serviceImpl/UserServiceImpl.java
@@ -6,14 +6,8 @@ import fhq.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -24,9 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
-
+    @Override
     public Map<String, Object> checkUserName(String username) {
-      Map<String,Object> map=new HashMap<String, Object>();
+      Map<String,Object> map=new HashMap<>();
       //对用户名进行判断
         if (username==null||"".equals(username)){
             map.put("state",0);
@@ -49,10 +43,10 @@ public class UserServiceImpl implements UserService {
 
     }
 
-
+    @Override
     public Map<String, Object> toReg(String username, String password) {
 
-        Map<String,Object> map=new HashMap<String, Object>();
+        Map<String,Object> map=new HashMap<>();
         //对参数进行判断
         if (username==null||"".equals(username)){
             map.put("state",100);
@@ -98,9 +92,9 @@ public class UserServiceImpl implements UserService {
 
     }
 
-
+    @Override
     public Map<String, Object> toLog(String username, String password, HttpServletRequest req) {
-        Map<String,Object> map=new HashMap<String, Object>();
+        Map<String,Object> map=new HashMap<>();
         //对参数进行判断
         if (username==null||"".equals(username)){
             map.put("state",100);
@@ -137,190 +131,13 @@ public class UserServiceImpl implements UserService {
                 map.put("msg","用户名或密码错误! (︶︿︶)");
                 return map;
             }
-        }
-
-    @Override
-    public Map<String, Object> toUpdatePassword(String oldPassword, String newPassword,
-                                                String confirmPassword,HttpServletRequest request) {
-        Map<String,Object> map=new HashMap<>();
-        //非空判断
-        if (oldPassword==null||"".equals(oldPassword)){
-            map.put("state",100);
-            map.put("msg","原密码不能为空");
-            return map;
-        }
-        if (newPassword==null||"".equals(newPassword)){
-            map.put("state",100);
-            map.put("msg","新密码不能为空");
-            return map;
-        }
-        if (confirmPassword==null||"".equals(confirmPassword)){
-            map.put("state",100);
-            map.put("msg","确认密码不能为空");
-            return map;
-        }
-        //通过用户名查找用户
-      String username = (String)request.getSession().getAttribute("username");
-
-        if (username==null){
-            map.put("state",200);
-            map.put("msg","用户名过期,请重新登录");
-            return map;
-        }
-        User user = userMapper.findUserByUserName(username);
-        //获取用户信息的盐值
-        String salt = user.getSalt();
-        //使用原密码和盐值进行加密
-        String md5Password = getMD5Password(oldPassword, salt);
-        //比对密码
-        if (!md5Password.equals(user.getUserPassword())){
-            map.put("state",100);
-            map.put("msg","原密码不一致");
-            return map;
-        }
-        //比较新密码和确认密码
-        if (!newPassword.equals(confirmPassword)){
-            map.put("state",100);
-            map.put("msg","密码不一致");
-            return map;
-        }
-        //给新密码加密
-        String newSalt = UUID.randomUUID().toString();
-        String md5PasswordNew = getMD5Password(newPassword, newSalt);
-
-        //使用持久层对象调用持久层方法
-        int row = userMapper.toUpdatePassword(md5PasswordNew,username,newSalt);
-        if (row>=1){
-            map.put("state",200);
-            map.put("msg","密码修改成功");
-            return map;
-        }else{
-            map.put("state",100);
-            map.put("msg","服务器异常,密码修改失败");
-            return map;
-        }
-    }
-
-    @Override
-    public User findUserInfoByUsername(String username) {
-        User user = userMapper.findUserInfoByUsername(username);
-        return user;
-    }
-
-    @Override
-    public Map<String, Object> modifyUserByName(String userName, String address, String tel, String sex,
-                                                String birthday,String edu,String signature,String industry,String email) {
-
-        Map<String,Object> map=new HashMap<>();
-        if (birthday==""){
-        birthday="1970-01-01";
-        }
-        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
-
-        Date date = null;
-        try {
-            date = ft.parse(birthday);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        User user=new User();
-        user.setUserName(userName);
-        user.setAddress(address);
-        user.setTel(tel);
-        user.setSex(sex);
-        user.setBirthday(date);
-        user.setEdu(edu);
-        user.setSignature(signature);
-        user.setIndustry(industry);
-        user.setEmail(email);
-        if ("".equals(user.getUserName())){
-            map.put("state",201);
-            map.put("msg","用户名已过期,请重新登录");
-            return map;
-        }
-        int row = userMapper.modifyUserInfoByUsername(user);
-        if (row>=1){
-            map.put("state",200);
-            map.put("msg","修改成功");
-            return map;
-        }else{
-            map.put("state",100);
-            map.put("msg","修改失败");
-            return map;
-        }
-
-    }
-
-    @Override
-    public Map<String, Object> uploadFace(MultipartFile file, HttpServletRequest request) {
-        Map<String,Object> map=new HashMap<>();
-        //参数判断
-        if (file==null||"".equals(file)){
-            map.put("msg","请选择上传文件");
-            return map;
-        }
-        //判断文件大小
-        if (file.getSize()>52428800){
-            map.put("state",100);
-            map.put("msg","文件大小超出50M");
-            return map;
-        }
-        //获取用户名
-        String username =(String) request.getSession().getAttribute("username");
-        System.out.println(username+12131);
-        if ("".equals(username)||username==null){
-            map.put("state",201);
-            map.put("msg","用户名已过期,请重新登录");
-            return map;
-        }
-//        //获取上传文件的原名
-//        String filename = file.getOriginalFilename();
-//        //从后往前找文件名的第一个"."
-//      int start = filename.lastIndexOf(".");
-        //得到文件的后缀
-        String subffix = ".jpg";
-                //filename.substring(start);
-        //使用UUID产生文件名前缀
-        String preffix = UUID.randomUUID().toString();
-        //新的文件名
-        String newFileName=preffix+subffix;
-        //文件存储在哪里
-        String path = request.getSession().getServletContext().getRealPath("face");//获取项目根目录
-        File f=new File(path);
-        if (!f.exists()){
-            f.mkdir();//将文件夹创建出来
-        }
-        //
-        File filePath=new File(path,newFileName);
-
-        try {   //实现上传到服务器
-            file.transferTo(filePath);
-
-            //头像路径
-            String face="/face/"+newFileName;
-
-            int row = userMapper.uploadFace(username, face);
-            if (row>=1){
-                map.put("state","200");
-                map.put("msg","上传成功");
-                map.put("face",face);
-                request.getSession().setAttribute("face",face);
-                return map;
-            }else {
-                map.put("state","100");
-                map.put("msg","上传失败,请重试");
-                return map;
-            }
 
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            map.put("state","100");
-            map.put("msg","上传失败");
-            return map;
+
         }
 
-    }
+
+
 
 
     //封装一个密码加密的方法
